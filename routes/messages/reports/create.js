0a81f17fd9c0c0bd1102a89bea07b5832350af6d
Anthony Kinsey <anthony@slickage.com>
2016-08-31 10:26:45 -1000

    Catch error or report creation

--- routes/messages/reports/create.js
+++ routes/messages/reports/create.js
@@ -1,4 +1,5 @@
 var Joi = require('joi');
+var Boom = require('boom');
 
 /**
   * @apiVersion 0.4.0
@@ -39,7 +40,9 @@ module.exports = {
   },
   handler: function(request, reply) {
     var report = request.payload;
-    var promise = request.db.reports.createMessageReport(report);
+    var promise = request.db.reports.createMessageReport(report)
+    .error(function(err) { return Boom.badRequest(err); });
+
     return reply(promise);
   }
 };
